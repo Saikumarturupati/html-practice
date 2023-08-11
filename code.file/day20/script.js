227286c2871cdaa294dd85931645d44927440e8d
Saikumarturupati <saikumarturupati@gmail.com>
2023-08-11 22:36:56 +0530

    first commit

---
+++ code.file/day20/script.js
@@ -0,0 +1,112 @@
+let js = "amazing";
+      /* if (js === "amazing") alert("java script babblu di!");
+      /*46+8+25-10;*/
+      /*console.log(46+8+25-10);*/
+      /*console.log("jonson");
+      console.log(16);
+      let PI = 3.145;*/
+      
+      // if (js === "amazing") alert("java script babblu !");
+    /*  let myfirstjob = "coder";
+      let mycurrentjob = "teacher";
+
+      let job1 = "programmer";
+      let job2 = "teacher";
+
+       
+       let javascriptfun = true;
+       console.log(myfirstjob);     
+
+      console.log(typeof true);
+      console.log(javascriptfun);
+      console.log(typeof 23);
+      console.log(typeof 'jonas');
+
+      javascriptfun = 'YES!';
+      console.log(typeof javascriptfun);
+
+      let year;
+      console.log(year);
+      console.log(typeof year);
+
+      year = 1991;
+      console.log(typeof year);
+
+      console.log(typeof null);*/
+      
+       /*if (js === "amazing") alert("java script babblu !");
+       let age = 30;
+       age = 31;
+       const birthyear = 1991;
+      //  birthyear = 1990 - error* 
+
+      /*const job;
+      var job = 'programmer';
+      job = 'teacher';*/
+
+      /*lastname   = 'schmedtmann';
+      console.log (lastname);*/
+
+      // MATHS
+      /*const now = 2037;
+      const agejonas = 2037 - 1999;
+      const agesarah = 2037 - 2018
+      console.log(agejonas,agesarah);
+      console.log(agejonas * 2,agesarah / 2**3)*/
+      // 2**3 means 2 to the power of 3 = 2*2*2
+
+      /*const firstname = 'jonas';
+      const lastname = 'saikumar';
+      console.log(firstname,lastname);*/
+
+      /*let x = 10 + 5;
+      x += 5;
+      console.log(x);*/
+
+      // console.log(ageJonas > agesaikumar);
+      // console.log(agesaikumar >= 18);
+      // const isfullAge = agesaikumar >= 18;
+      // console.log(now -1991 > now - 2018);
+
+      /*const now = 2018;
+      const agesaikumar = now - 1991;
+      const agebabblu = now - 2020;
+      console.log(now-1991 > now-2020);*/
+
+      /*console.log(25-10-5)*/
+
+      /*let x,y;
+      x = y = 25-10-5
+      console.log(x,y)*/
+
+      /*const overage = (agebabblu+agesaikumar);
+      // console.log(agebabblu,agesaikumar,averageAge);
+
+      /*1*Store marks and john's mass and height in variables
+      2*caluclate both their BMIs using the formala (you can even implement both version0)
+      3* Create a bollean variable 'MarkHeight BMI'
+      Containing information about wheher mark has a higher BMI than John.BMI
+
+      TEST DATA1 : Marks weight 78kg and is 1.69m Tall
+         John weight 92kg and is 1.95m TALL
+      TEST DATA2 : MarkWeight 95kg and 1.88m TAil.
+           John weight 85kg and is 1.76m TAIL.*/
+
+      // const MassMArk = 78;
+      // const HeightMArk = 1.69;
+      // const MassJOhn = 92;
+      // const HeightJOhn = 1.95;
+
+      // const BMIMARk = Massmark;
+      // const BMIJohn = Massjohn;
+      // const MarkheigherBMI = BMIMark > BMIJohn;
+      // console.log (BMIMark , BMIJoh);
+
+      /*const firstname = 'John';
+      //const job = 'teacher';
+      const birthyear = 1991;
+      const year = 2037;
+      const john = "I'm" + firstname + : a' + ( Year - birthyear) + 'years old' + job + 
+      console.log (john);*/
+
+      
